f71bdcab1522e6abf10752a08007d321549ca56f
Kempy02 <76395742+Kempy02@users.noreply.github.com>
2022-05-30 14:51:55 +1000

    Update A2part2.m

--- A2part2.m
+++ A2part2.m
@@ -74,7 +74,7 @@ xlabel("Time [s]"), ylabel("Amplitude"), title("Periodic Noise Signal (First 3 s
 tNoise = linspace(0, noisePeriod, length(Noisesig)+1); tNoise(end) = [];
 ts = tNoise(2)-tNoise(1); 
 f0 = 1/noisePeriod;
-N = 6;
+N = 15;
 n = (-N:N).';
 cn = f0 * Noisesig * exp(-1j*2*pi*f0*n*tNoise).' * ts;
 c0 = cn(N+1);
@@ -88,7 +88,7 @@ NoisesigApprox = cn * exp(1j*2*pi*n*f0*tNoise);
 cn(N+1) = 0;
 
 %% 2.7
-Noisesig_fs = cn * exp(1j*2*pi*n*f0*tNoise);
+Noisesig_fs = cn * exp(1j*2*pi*f0*n*tNoise);
 
 %% 2.8
 figure(3), hold on;
@@ -100,7 +100,42 @@ legend("Noisesig", "NoisesigApprox", "Noisesig\_fs")
 % Looks like some of the more detailed noise at the peaks and valleys are
 % not modelled by only 6 harmonics. Might be worth using 10 to get these (9
 % has better overall shape but 10 gets the little bits heaps good)
+
 %% 2.9
+
+t_tot = length(t)*ts / noisePeriod;                 % no. of periods in total
+Noisesig_fs = repmat(Noisesig_fs, [1, ceil(t_tot)]);    % fourier approx of periodic noise signal repeated over total time (rounded up)
+Noisesig_fs = Noisesig_fs(1:length(t));                % fourier approx repeated over total time (exact)
+
+im1(1,:) = sig(1,:) - Noisesig_fs;                      % de-noised signal
+ 
+figure(4)
+imshow(reshape(im1(1,:), h, w))
+
 %% 2.10
+
+% evaluate the fourier transform of hdm
+IM1(1,:) = fft(im1(1,:));                                                     %fourier transform of demodulated hiddenSignal
+
+% define bandwidth
+B_upper = 223;                                                              % upper noise bandwidth
+B_lower = 240;                                                              % lower noise bandwidth
+B_upper_i = B_upper*(length(sig(1,:))*ts);                                  %upper index value 
+B_lower_i = B_lower*(length(sig(1,:))*ts);                                  %lower index value
+middle_f = length(sig(1,:))/2;
+
+% create vector to filter signal
+filter = ones(1, length(f));                                                % ones vector
+filter(middle_f - B_upper_i : middle_f - B_lower_i) = 0;                    % filter
+filter(middle_f + B_lower_i : middle_f + B_upper_i) = 0;                    % filter
+
+% filter HDM with filter vector k_filter
+filtered = IM1(1,:).*filter;                                        %filtered demodulated signal in frequency domain
+
+% compute inverse fourier transform to change to time domain
+im2(1,:) = ifft(filtered);     %%filtered demodulated signal in frequency domain
+
+figure(5)
+imshow(reshape(im2(1,:), h, w))
 %% 2.11
 %% 2.12
